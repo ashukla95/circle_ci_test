d186b3f7959295a12b612c1a6cad52a70cae1881
ashukla95 <shukla.ais@husky.neu.edu>
2020-11-10 23:57:41 -0500

    added config for tests.

---
+++ test/test_add.spec.ts
@@ -0,0 +1,13 @@
+import { expect } from "chai";
+import add from "../addition/add";
+
+
+describe("add function test suite: ", function () {
+    it("returns 5 for 2+3: ", function () {
+        expect(add(2, 3)).to.equal(5);
+    });
+
+    it("5 != 4", function () {
+        expect(add(2, 3)).to.not.equal(4);
+    })
+})
